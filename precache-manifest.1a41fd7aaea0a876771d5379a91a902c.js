cf644ac72602495fc84980fe520f4dc6b025459d
Rike <hello@ueberdiespree.de>
2019-08-07 17:10:15 +0200

    Updates

---
+++ precache-manifest.1a41fd7aaea0a876771d5379a91a902c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2f419fff3dd968b1a9539068b8aee7",
+    "url": "/cradle-to-cradle/index.html"
+  },
+  {
+    "revision": "56fb1ecdc16453a59b05",
+    "url": "/cradle-to-cradle/static/css/main.f3fbb79e.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/cradle-to-cradle/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "56fb1ecdc16453a59b05",
+    "url": "/cradle-to-cradle/static/js/main.0ad9e0c5.chunk.js"
+  },
+  {
+    "revision": "e994100502cf198aedd1",
+    "url": "/cradle-to-cradle/static/js/runtime~main.156a51f6.js"
+  }
+]);
